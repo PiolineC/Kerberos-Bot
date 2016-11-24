69c02beadc480e41334f860c80ee95b3abd8c2af
YordleLover97 <pxchoudhury@gmail.com>
2016-11-24 11:56:19 -0500

    :hammer: Refactor >rename
    Now features robust error handling and takes advantage of regexp.

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ bot.on('ready', () => {
 bot.on('message', msg => {
 	let prefix = '>',
 	input = msg.content,
-	sender = msg.author;
+	sender = msg.author,
+	channel = msg.channel;
 
 	/* 
 		Exit and stop if message doesn't start with prefix
@@ -21,22 +22,44 @@ bot.on('message', msg => {
 	if(!input.startsWith(prefix)) return;
 	if(sender.bot) return;
 
-	if (input.startsWith(prefix + 'rename')) {
-		try {		
-			console.log(input.substring(prefix.length + 7));
-			let args = input.substring(prefix.length + 7).split('"');
-			console.log(args);
-			let query = args[1];
-			console.log(query);
-			let name = args[3];
-			console.log(name);
+	if (input.startsWith(prefix + 'rename')) {				
+		input = input.replace(prefix + 'rename ', ''); //trim away command
 
-			let match = bot.channels.find('name', query);
-			match.setName(name);
+		let quotePattern = /".*?"/,
+		validCharacterPattern = /^[a-zA-Z0-9_-]+$/;
+		
+		//check for first argument
+		if (quotePattern.test(input)) { 
+			try {
+				let query = (quotePattern.exec(input))[0].replace(/"/g,''),			
+				match = bot.channels.find('name', query); //find and store corresponding channel
+				input = input.substring(query.length+2); //trim away first argument			
 
-		} catch (err) {	
-			msg.channel.sendMessage('Channel not found.'); 
-		}
+				//check for second argument
+				if (quotePattern.test(input)) {
+					let name = (quotePattern.exec(input))[0].replace(/"/g,'');
+					//check to make sure desired name fits discord's naming criteria
+					if (name.length < 2 || name.length > 100) 
+						channel.sendMessage('Channel names must be between 2-100 characters long.'); 
+					else if (match.type === 'text' && !validCharacterPattern.test(name))
+						channel.sendMessage(
+							'Text channel names must be alphanumeric with dashes or underscores.');
+					else {
+						match.setName(name);
+						channel.sendMessage(
+							'Channel "' + query + '" successfully renamed to "' + name + '"!');
+					}
+
+				//executes if no second argument is found
+				} else { channel.sendMessage('Please input your desired name change.'); }
+
+			//executes if parsed channel name doesn't exist
+			} catch (err) { channel.sendMessage('Channel not found.'); }
+
+		} else { //executes if no arguments are found
+			channel.sendMessage(
+				'Please input the channel you wish to rename and your desired name change.');
+		}				
 	} 
 });
 
