18b5fb7d538a9ced3dd05a47932fd2180a633cd9
YordleLover97 <pxchoudhury@gmail.com>
2016-12-05 21:14:10 -0500

    :hammer: Refactor rename command
    Cleaned up the code of the rename command to be less monolithic and employ the use of specialized helper functions.

--- rename.js
+++ rename.js
@@ -11,49 +11,71 @@
 
 'use strict';
 
+//searches an input for two arguments wrapped in double quotes ("")
+function getArgs(input) {
+	const quotePattern = /".*?"/g;
+	const args = input.match(quotePattern);
+
+	if (!args)  
+		return { errorMessage: 'Please input the channel you wish to rename and your desired name change.' };
+	else if (args.length < 2) 
+		return { errorMessage: 'Please input your desired name change.' };
+	else if (args[0] === args[1])
+		return { errorMessage: 'Channel name already matches desired name.' };
+	else {
+		return { 
+			query: args[0].replace(/"/g,''),
+			name: args[1].replace(/"/g,'')
+		};
+	}
+}
+
+//checks to see if desired name change adheres to Discord's naming criteria
+function checkName(name, type) {
+	const validCharacterPattern = /^[a-zA-Z0-9_-]+$/;
+	if (name.length < 2 || name.length > 100) 
+		return { errorMessage: 'Channel names must be between 2-100 characters long.' }; 
+	else if (type === 'text' && !validCharacterPattern.test(name))
+		return { errorMessage: 'Text channel names must be alphanumeric with dashes or underscores.' }; 
+	else 
+		return { valid: true };
+}
+
+//searches server for a channel matching the provided query 
+function findMatch(channels, query) {
+	return channels.find('name', query);
+}
+
 module.exports = {
 	name: 'rename',
 	description: 'Change the name of a channel.',
-	help: 'rename "currentName" "desiredName"',
+	usage: 'rename [current] [desiredName]',
 	process: function(bot, channel, input) {
-		let quotePattern = /".*?"/,
-		validCharacterPattern = /^[a-zA-Z0-9_-]+$/;
+		const args = getArgs(input);
 
-		//check for first argument
-		if (quotePattern.test(input)) { 
-			try {
-				let query = (quotePattern.exec(input))[0].replace(/"/g,''),			
-				match = bot.channels.find('name', query); //find and store corresponding channel
-				//trim away first argument and any leading text
-				input = input.substring(input.indexOf(query) + query.length + 2); 
+		if (args.errorMessage)
+			channel.sendMessage(args.errorMessage);
 
-				//check for second argument
-				if (quotePattern.test(input)) {
-					let name = (quotePattern.exec(input))[0].replace(/"/g,'');
+		else {
+			const query = args.query;
+			const name = args.name;
+			const match = findMatch(bot.channels, query);
 					
-					//check to make sure desired name fits discord's naming criteria
-					if (name.length < 2 || name.length > 100) 
-						channel.sendMessage('Channel names must be between 2-100 characters long.'); 
-					else if (match.type === 'text' && !validCharacterPattern.test(name))
-						channel.sendMessage(
-							'Text channel names must be alphanumeric with dashes or underscores.');
-					else if (name === query)
-						channel.sendMessage('Channel name is already "' + name + '"!');
-					else {
-						match.setName(name);
-						channel.sendMessage(
-							'Channel "' + query + '" successfully renamed to "' + name + '"!');						
-					}
+			if (!match) 	
+				channel.sendMessage('Channel not found.')						
 
-				//executes if no second argument is found
-				} else { channel.sendMessage('Please input your desired name change.'); }
+			else { 
+				const status = checkName(name, match.type);
+				
+				if (!status.valid) 
+					channel.sendMessage(status.errorMessage); 		
 
-			//executes if parsed channel name doesn't exist
-			} catch (err) { channel.sendMessage('Channel not found.'); }
-
-		} else { //executes if no arguments are found
-			channel.sendMessage(
-				'Please input the channel you wish to rename and your desired name change.');
+				else { 
+					match.setName(name);
+					channel.sendMessage(
+						'Channel "' + query + '" successfully renamed to "' + name + '"!');	
+				}
+			}
 		}	
 	}
-}
+};
