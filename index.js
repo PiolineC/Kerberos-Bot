c6755343067a8866db936bf39da9f2a990e15e18
YordleLover97 <pxchoudhury@gmail.com>
2016-12-15 04:45:43 -0500

    :heavy_plus_sign::hammer: Refactor command modules to return Bluebird promises
    Command modules are now expected to return a promise.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 //create instance of a Discord Client 
-const Discord = require('discord.js'),
-bot = new Discord.Client();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
 
 //import settings from config.json
 const config = require('./init-config.js').init();
+const prefix = config.command_prefix;
 
 //initialize command modules
 const commands = 
@@ -12,30 +13,29 @@ const commands =
 		rename: require('./rename.js').process
 	};
 
-bot.on('ready', () => {
-	console.log('Ready!');
-});
+bot.on('ready', () => { console.log('Ready!'); });
 
 bot.on('message', msg => {
-	const prefix = config.command_prefix,	
-	input = msg.content,
-	sender = msg.author,
-	channel = msg.channel;
+	const input = msg.content;
+	const sender = msg.author;
+	const server = msg.guild;
+	const channel = msg.channel;
 
-	//Exit and stop if message doesn't start with prefix
-	//or if the message sender is a bot.
-	if(!input.startsWith(prefix)) return;
-	if(sender.bot) return;
+	if (!input.startsWith(prefix)) return;
+	if (channel.type !== 'text') return;
+	if (sender.bot) return;
 
-	if (input.startsWith(prefix + 'rename') || input.startsWith(prefix + 'rn')) {	
-		commands.rename(bot, channel, input);	
+	if (input.startsWith(prefix + 'rename') || input.startsWith(prefix + 'rn')) {
+		commands.rename(input, server.channels)
+		.then(output => { channel.sendMessage(output) })
+		.catch(err => { channel.sendMessage(err); });	
 	} 
 });
 
+//attempt login
 if (config.token)
 	bot.login(config.token);
 else {
-	console.log(
-		'Discord bot login token required to proceed. Please include one in your config.json.')
+	console.log('Discord bot login token required to proceed. Please include one in your config.json.');
 	process.exitCode = 1;
 }
--- rename.js
+++ rename.js
@@ -1,19 +1,39 @@
 /* 
-	This command allows users to change voice
-	or text channel	names through the bot. 
-	Bot *must* have "Manage Channels" permission.
 
-	Useful if you'd like memebers of your 
-	server to be able to edit channel names
-	without giving them access to the other 
-	permissions attached to managing channels.
+This command allows users to change voice
+or text channel	names through the bot. 
+Bot *must* have "Manage Channels" permission.
+
+Useful if you'd like memebers of your 
+server to be able to edit channel names
+without giving them access to the other 
+permissions attached to managing channels.
+
 */
 
 'use strict';
 
+const Promise = require('bluebird');
+
 const quotePattern = /"(.*?)"/g; 
 const shortcutPattern = /(v|t)\d+/; 
 const validCharacterPattern = /^[a-zA-Z0-9_-]+$/;
+
+function rename(input, channels) {
+	let name, oldName;
+	return getArgs(input)
+		.then(args => {
+			name = args.name;
+			return findMatch(args.query, channels);
+		})
+		.then(match => checkName(name, match))
+		.then(match => { 
+			oldName = match.name;
+			return match.setName(name);				
+		})	
+		.then(channel => Promise.resolve(`Channel "${oldName}" successfully renamed to "${channel.name}"!`))
+		.catch(err => Promise.reject(err));
+}
 
 //searches an input for two arguments
 function getArgs(input) {	
@@ -28,70 +48,42 @@ function getArgs(input) {
 		});		
 	}
 
-	if (!args)  
-		return { errorMessage: 'Please input the channel you wish to rename and your desired name change.' };
-	else if (args.length < 2) 
-		return { errorMessage: 'Please input your desired name change.' };
+	if (args.length === 0)  
+		return Promise.reject('Please input the channel you wish to rename and your desired name change.');
+	else if (args.length === 1) 
+		return Promise.reject('Please input your desired name change.');
 	else {
 		const query = args[0].shortcut ? args[0] : args[0].replace(/"/g,'');
 		const name = args[1].replace(/"/g,'');		
-		return { query: query, name: name };
+		return Promise.resolve({ query: query, name: name });
 	}
 }
 
 //checks to see if desired name change adheres to Discord's naming criteria
-function checkName(name, type) {	
+function checkName(name, channel) {	
 	if (name.length < 2 || name.length > 100) 
-		return { errorMessage: 'Channel names must be between 2-100 characters long.' }; 
-	else if (type === 'text' && !validCharacterPattern.test(name))
-		return { errorMessage: 'Text channel names must be alphanumeric with dashes or underscores.' }; 
+		return Promise.reject('Channel names must be between 2-100 characters long.');
+	else if (name === channel.name) 
+		return Promise.reject(`Channel name is already "${name}"!`);
+	else if (channel.type === 'text' && !validCharacterPattern.test(name))
+		return Promise.reject('Text channel names must be alphanumeric with dashes or underscores.'); 
 	else 
-		return { valid: true };
+		return Promise.resolve(channel);
 }
 
 //searches server for a channel matching the provided query 
-function findMatch(channels, query) {
-	if (!query.shortcut)
-		return channels.find('name', query);
-	else {
-		return channels.find(function(channel) {
-			return channel.type === query.type && channel.position === query.index;
-		});
-	}
+function findMatch(query, channels) {
+	let match;
+	if (query.shortcut)
+		match = channels.find(channel => channel.type === query.type && channel.position === query.index);
+	else
+		match = channels.find('name', query);
+	return match ? Promise.resolve(match) : Promise.reject('Channel' + (query.shortcut ? '' : ` "${query}"`) + ' not found.');	
 }
 
 module.exports = {
 	name: 'rename',
-	description: 'Change the name of a channel.',
-	usage: 'rename "from" "to"',
-	process: function(bot, channel, input) {
-		const args = getArgs(input);		
-		if (args.errorMessage)
-			channel.sendMessage(args.errorMessage);
-		else {
-			const name = args.name;
-			const query = args.query;
-			const match = findMatch(bot.channels, query);	
-					
-			if (!match) 	
-				channel.sendMessage('Channel' + (query.shortcut ? '' : ' "' + query + '" ' ) + ' not found.');			
-			else { 
-				const status = checkName(name, match.type);
-				if (!status.valid) 
-					channel.sendMessage(status.errorMessage); 
-				else { 
-					const oldName = match.name;
-					if (oldName === name)
-						channel.sendMessage('Channel name is already "' + oldName + '"!');
-					else {
-						match.setName(name)	
-						.then(function onSuccess() { 
-							channel.sendMessage('Channel "' + oldName + '" successfully renamed to "' + name + '"!'); 
-						})
-						.catch(function onError() { channel.sendMessage('Naming operation failed.'); });							
-					}				
-				}
-			}
-		}	
-	}
+	usage: '"from" "to"',
+	description: 'Change the name of a channel.',	
+	process: rename
 };
